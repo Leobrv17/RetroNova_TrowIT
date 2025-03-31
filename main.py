9aa88e40adc5ad6563b3597212184b22dff827d2
Leobrv17 <leo.breviere@gmail.com>
2025-03-31 12:16:20 +0200

    add main fonction

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-from gameTi import GameTi
-from screen import Screen
-from qrCode import QrCode
-from sound import Sound
 import time
 import pygame
 
-if __name__ == "__main__":
-    pygame.init()
+from gameTi import GameTi
+from sound import Sound
+
+def main(screen, twoPlayer):
+
     music = Sound()
     music.load_sound("ground", "./sound/touchGround.mp3")
     music.load_sound("wood", "./sound/touchWood.mp3")
     music.load_sound("throw", "./sound/throw.mp3")
 
-    screen = Screen()
-    qrCode = QrCode()
+    game = GameTi(screen, music, twoPlayer)
 
-    background_image = pygame.image.load("./assets/front.png").convert()
-    while 1:
-        game = GameTi(screen, music, False)
+    while game.screen.running and game.screen.state == "game":
         game.run()
         pygame.display.update()
         time.sleep(0.1)
+
+    # Récupération des scores après la fin de la partie
+    score_p1 = game.player1.score.get()
+    if not twoPlayer:
+        score_p2 = None
+    else:
+        score_p2 = game.player2.score.get()
+
+    return score_p1, score_p2
+
+
+if __name__ == "__main__":
+    from screen import Screen
+    screen = Screen()
+    score_p1, score_p2 = main(screen, True)
+    print(f"Score Joueur 1 : {score_p1}, Score Joueur 2 : {score_p2}")
